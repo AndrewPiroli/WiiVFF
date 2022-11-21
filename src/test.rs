b22adba816bbb6f571a38ee063c58c3f0e2da853
Andrew Piroli <drewpiroli@gmail.com>
2022-11-21 12:28:24 -0500

    Improve tests

--- src/test.rs
+++ src/test.rs
@@ -5,18 +5,13 @@ fn open() -> Result<std::fs::File> {
 }
 
 #[test]
-pub fn debug_vff() -> Result<()> {
+pub fn vff_header() -> Result<()> {
     let f = open()?;
     let (vff, _) = VFF::new(f)?;
-    println!("{vff:?}");
-    Ok(())
-}
-    
-#[test]
-pub fn debug_root_dir() -> Result<()> {
-    let f = open()?;
-    let (_, root_dir) = VFF::new(f)?;
-    println!("{root_dir:?}");
+    let header = &vff.borrow().header;
+    assert_eq!(header.volume_size, 0x1400000);
+    assert_eq!(header.cluster_size, 0x200);
+    assert_eq!(header.cluster_count, 0xa000);
     Ok(())
 }
 
@@ -24,15 +19,41 @@ pub fn debug_root_dir() -> Result<()> {
 pub fn ls_root_dir() -> Result<()> {
     let f = open()?;
     let (_, root_dir) = VFF::new(f)?;
-    let a = root_dir.ls(None, None)?;
-    dbg!(a);
+    let root_dir_contents = root_dir.ls(None, None)?;
+    assert_eq!(root_dir_contents.len(), 2);
+    assert!(root_dir_contents.contains(&"/CDB~1.CON [0x0004]".to_owned()));
+    assert!(root_dir_contents.contains(&"/2022/10/15/21/44/HAEA_#1/LOG/2B06C4C3.000 [0x0ca0]".to_owned()));
     Ok(())
 }
 
 #[test]
 pub fn dump_root() -> Result<()> {
     let f = open()?;
+    let temp_dir = std::env::temp_dir().to_string_lossy().into_owned() + "/WiiVFF-tests";
+    if std::path::Path::new(&temp_dir).exists() {
+        std::fs::remove_dir_all(&temp_dir)?;
+    }
     let (_, root_dir) = VFF::new(f)?;
-    root_dir.ls(None, Some("/tmp".to_owned()))?;
+    root_dir.ls(None, Some(temp_dir.clone()))?;
+    let file1 = temp_dir.clone() + "/CDB~1.CON";
+    let mut cdb_file: Vec<u8> = Vec::with_capacity(0x4);
+    let cdb_file_size = File::open(file1)?.read_to_end(&mut cdb_file)?;
+    assert_eq!(cdb_file_size, 0x4);
+    assert_eq!(cdb_file, vec![0;4]);
+
+    let file2_compare = "test_data/compare.bin".to_owned();
+    let mut file2_compare_data: Vec<u8> = Vec::with_capacity(0xca0);
+    let file2_compare_size = File::open(file2_compare)?.read_to_end(&mut file2_compare_data)?;
+
+    let file2 = temp_dir.clone() + "/2022/10/15/21/44/HAEA_#1/LOG/2B06C4C3.000";
+    let mut playlog_file: Vec<u8> = Vec::with_capacity(0xca0);
+    let playlog_file_size = File::open(file2)?.read_to_end(&mut playlog_file)?;
+    assert_eq!(playlog_file_size, 0xca0);
+    assert_eq!(file2_compare_size, playlog_file_size);
+    assert_eq!(playlog_file, file2_compare_data);
+
+    if std::path::Path::new(&temp_dir).exists() {
+        std::fs::remove_dir_all(&temp_dir)?;
+    }
     Ok(())
 }
